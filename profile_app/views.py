ea8dd3e1fbc29e81df7b8875ba71ac9160c71943
potherpothik <smahmud@live.com>
2023-12-03 20:20:49 +0600

    sesume

---
+++ profile_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def index(request):
+  return render(request, 'index.html')
+
+def contact(request):
+  return render(request, 'contact.html')
+
+def projects(request):
+  return render(request, 'projects.html')
+
+def resume(request):
+  return render(request, 'resume.html')
+
+def base(request):
+  return render(request, 'base.html')
